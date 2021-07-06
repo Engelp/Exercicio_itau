0a6aeddd149b8ac4e6b7ccf9302a8f5f9bd27c26
Caio Antunes <caio.antuns@gmail.com>
2021-07-06 18:29:03 -0300

    Atualização 5

--- Desafio/Exercicio05.cs
+++ Desafio/Exercicio05.cs
@@ -6,7 +6,7 @@ namespace Exercicio_itau
     {
         public static void Executar()
         {
-            double bruto, liquido, desconto, hora, htrab, perct, perct2 = 0.5, perct3 = 0.1, perct4 = 0.2;
+            double bruto, liquido, desconto, hora, htrab, perct, perct2 = 0.05, perct3 = 0.1, perct4 = 0.2;
             liquido = 0;
             perct = 0;
             
@@ -20,6 +20,7 @@ namespace Exercicio_itau
 
             if (bruto <= 2000)
             { 
+                liquido = bruto;
                 perct = 0;
             }
             else if (bruto <= 5000) 
@@ -27,27 +28,25 @@ namespace Exercicio_itau
                 liquido = bruto - (bruto * perct2);
                 perct = perct2;
             }
-            if (bruto <= 7500) 
+            else if (bruto <= 7500)
             {
                 liquido = bruto - (bruto * perct3);
                 perct = perct3;
-                {
-                }
-                if (bruto > 7500)
-                {
+            } else {
                     liquido = bruto + (bruto * perct4); 
                     perct = perct4;
-                }
-            }
+                    }
+                
+            
             desconto = bruto - liquido ;
             //perct = Convert.ToDouble(())
-            Console.WriteLine("Salário Bruto: (" + hora + " * " + htrab + ")                               : R$  " + bruto);
-            Console.WriteLine("(-) IR (" + (perct * 100) +"%)                                           : R$  " +  (bruto * perct));
-            Console.WriteLine("(-) INSS ( 10%)                                        : R$  " + (bruto * 0.1));
-            Console.WriteLine("FGTS (11%)                                             : R$  " + (bruto * 0.11));
-            Console.WriteLine("Sindicato (3%)                                         : R$  " + (bruto * 0.03));
-            Console.WriteLine("Total de descontos                                     : R$  " + ((bruto * 0.24 + (bruto * perct))));                                   
-            Console.WriteLine("Salário líquido                                        : R$  " + (liquido - ((bruto * 0.24)+(bruto*perct))));
+            Console.WriteLine("Salário Bruto: (" + hora + " * " + htrab + ")                              : R$   " + bruto);
+            Console.WriteLine("(-) IR (" + (perct * 100) +"%)                                           : R$   " +  (bruto * perct));
+            Console.WriteLine("(-) INSS ( 10%)                                        : R$   " + (bruto * 0.1));
+            Console.WriteLine("FGTS (11%)                                             : R$   " + (bruto * 0.11));
+            Console.WriteLine("Sindicato (3%)                                         : R$   " + (bruto * 0.03));
+            Console.WriteLine("Total de descontos                                     : R$   " + ((bruto * 0.24 + (bruto * perct))));                                   
+            Console.WriteLine("Salário líquido                                        : R$   " + (liquido - ((bruto * 0.24)+(bruto*perct))));
         }
     }
 }
