f5214a7e424d1dbc6bb47ca9ff70ba9874e10d2b
Caio Antunes <caio.antuns@gmail.com>
2021-07-06 13:17:15 -0300

    Primeiro Up

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio_itau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Lista de Exercícios ****");
+
+            
+        }
+    }
+}
---
+++ obj/Desafio/Exercicio01.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Exercicio_itau
+{
+    static class Program
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("**** Exercício 01 ****");
+
+            
+        }
+    }
+}
